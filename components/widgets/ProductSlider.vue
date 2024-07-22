078d97719f3c64dd9e4fd8f6883060407681ac2b
John Doe <johndoe@example.com>
2024-07-22 13:25:54 +0300

    fixed: слайдеры на баннера категорий +  мобилки

--- components/widgets/ProductSlider.vue
+++ components/widgets/ProductSlider.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 import { SwiperSlide, Swiper } from "swiper/vue";
-
 const prev = ref(null);
 const next = ref(null);
+const bannerPrev = ref(null);
+const bannerNext = ref(null);
 
 const props = defineProps(["data", "category"]);
 const route = useRoute();
@@ -20,14 +21,55 @@ onMounted(async () => {
   <!-- {{ category.products }} -->
   <section class="product-slider" v-if="category.products.length > 0">
     <div class="banner" v-if="category?.category_banners.length > 0">
-      <div class="banner-desk">
-        <NuxtImg :src="category?.category_banners[0].image" alt="" />
+      <Swiper
+        class="swiper"
+        :pagination="true"
+        :modules="[Pagination, Navigation]"
+        :navigation="{
+          prevEl: bannerPrev,
+          nextEl: bannerNext,
+        }"
+      >
+        <SwiperSlide v-for="item in category?.category_banners" :key="item.id">
+          <div class="banner-desk">
+            <NuxtImg :src="item.image" alt="" />
+          </div>
+
+          <div class="banner-mob">
+            <img :src="item.image_mob" alt="" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+
+      <div ref="bannerPrev" class="banner-arrow-prev">
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="white"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M5.69997 8.00105L10.6 12.9011C10.7666 13.0677 10.8472 13.2622 10.8416 13.4844C10.8361 13.7066 10.75 13.9011 10.5833 14.0677C10.4166 14.2344 10.2222 14.3177 9.99997 14.3177C9.77774 14.3177 9.5833 14.2344 9.41663 14.0677L4.2833 8.95105C4.14997 8.81772 4.04997 8.66772 3.9833 8.50105C3.91663 8.33438 3.8833 8.16772 3.8833 8.00105C3.8833 7.83438 3.91663 7.66772 3.9833 7.50105C4.04997 7.33438 4.14997 7.18438 4.2833 7.05105L9.41663 1.91772C9.5833 1.75105 9.78052 1.67049 10.0083 1.67605C10.2361 1.68161 10.4333 1.76772 10.6 1.93438C10.7666 2.10105 10.85 2.29549 10.85 2.51772C10.85 2.73994 10.7666 2.93438 10.6 3.10105L5.69997 8.00105Z"
+          />
+        </svg>
       </div>
 
-      <div class="banner-mob">
-        <img :src="category?.category_banners[0].image_mob" alt="" />
+      <div ref="bannerNext" class="banner-arrow-next">
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="white"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.3167 8.0013L5.41674 3.1013C5.25007 2.93464 5.16952 2.73741 5.17507 2.50964C5.18063 2.28186 5.26674 2.08464 5.43341 1.91797C5.60007 1.7513 5.7973 1.66797 6.02507 1.66797C6.25285 1.66797 6.45007 1.7513 6.61674 1.91797L11.7334 7.0513C11.8667 7.18464 11.9667 7.33464 12.0334 7.5013C12.1001 7.66797 12.1334 7.83464 12.1334 8.0013C12.1334 8.16797 12.1001 8.33463 12.0334 8.5013C11.9667 8.66797 11.8667 8.81797 11.7334 8.9513L6.60007 14.0846C6.43341 14.2513 6.23896 14.3319 6.01674 14.3263C5.79452 14.3207 5.60007 14.2346 5.43341 14.068C5.26674 13.9013 5.18341 13.7041 5.18341 13.4763C5.18341 13.2485 5.26674 13.0513 5.43341 12.8846L10.3167 8.0013Z"
+          />
+        </svg>
       </div>
     </div>
+
     <div class="container">
       <div class="product-slider__top">
         <h2 class="product-slider__title">{{ category.title }}</h2>
@@ -112,15 +154,18 @@ onMounted(async () => {
 }
 
 .banner {
-  aspect-ratio: 16/9;
   overflow: hidden;
-
+  position: relative;
   img {
     object-fit: cover;
     width: 100%;
     display: block;
     height: 100%;
   }
+}
+
+.banner-desk {
+  aspect-ratio: 16/9;
 }
 
 .banner-mob {
@@ -216,7 +261,7 @@ onMounted(async () => {
 @media screen and (max-width: 768px) {
   .product-slider__top {
     justify-content: space-between;
-    margin-top: 0;
+    margin-top: 24px;
   }
 
   .product-slider__wrap {
@@ -224,16 +269,12 @@ onMounted(async () => {
     margin-top: 16px;
   }
 
-  .banner {
-    margin-bottom: 8px;
-    // border-radius: 4px;
-  }
-
   .banner-desk {
     display: none;
   }
   .banner-mob {
     display: block;
+    aspect-ratio: 9 / 16;
   }
 }
 </style>
