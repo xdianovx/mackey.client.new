841b734d41bc2b0c33d8fcd7195a413fdd03a676
John Doe <johndoe@example.com>
2024-08-15 23:05:14 +0300

    asdasd

--- components/sections/Index/Hero.vue
+++ components/sections/Index/Hero.vue
@@ -42,7 +42,6 @@ const options = {
 
             <video
               v-if="useGetFileExtention(item.image) == 'mp4'"
-              data-autoplay
               playsinline
               autoplay
               loop
@@ -54,7 +53,6 @@ const options = {
 
             <video
               v-if="useGetFileExtention(item.image) == 'mp4'"
-              data-autoplay
               playsinline
               autoplay
               loop
