b3fa0e40fb9c73ae196ecc2a568414cf7147f4f3
John Doe <johndoe@example.com>
2024-09-03 16:39:50 +0300

    фыв

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -54,6 +54,7 @@ export default defineNuxtConfig({
     "@fullpage/nuxt-fullpage",
     "nuxt-yandex-metrika",
     "@nuxtjs/tailwindcss",
+    "@zadigetvoltaire/nuxt-gtm",
     "nuxt-gtag",
     [
       "yandex-metrika-module-nuxt3",
@@ -86,6 +87,20 @@ export default defineNuxtConfig({
   sitemap: {
     sources: ["/api/sitemap"],
   },
+
+  gtm: {
+    id: "GTM-5WF2C8R6", // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
+
+    defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
+    compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`
+    enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+    debug: true, // Whether or not display console logs debugs (optional)
+    loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+    enableRouterSync: true, // Pass the router instance of your app to automatically sync with router (optional)
+    trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
+    devtools: true, // (optional)
+  },
+
   build: {
     transpile: ["gsap", "vue-sonner"],
   },
